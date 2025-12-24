e8245e35e7512f8a53154cd50af1e3351bb3d959
Levente Szabadkai <levente@szabadkai.com>
2025-12-24 14:03:47 +0100

    fix: version number

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { readFileSync } from "fs";
+
+const pkg = JSON.parse(readFileSync("./package.json", "utf-8"));
 
 export default defineConfig({
     base: "./",
@@ -8,8 +11,6 @@ export default defineConfig({
         outDir: "dist",
     },
     define: {
-        __BUILD_VERSION__: JSON.stringify(
-            process.env.VITE_BUILD_VERSION || "dev"
-        ),
+        "import.meta.env.VITE_APP_VERSION": JSON.stringify(pkg.version),
     },
 });
